c7e8b3bd782d694acfb18b30311bf31f83e1c398
Nahom <zerihunnahom@gmail.com>
2023-04-04 20:13:51 +0300

    Fix import error

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { Provider } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
 import store from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
