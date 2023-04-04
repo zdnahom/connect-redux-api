63d3fa1cb95d2454d1fd54414ab8a5a7de607a39
Nahom <zerihunnahom@gmail.com>
2023-04-04 20:06:12 +0300

    Provide store to App component

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import store from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>
